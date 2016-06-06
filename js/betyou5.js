e87ca5fe5c61abe4bec03d4ae827681ab2d20dbc
Daniel T <toro_duque@hotmail.com>
2016-06-06 21:46:17 +1000

    Dice game in angularjs

---
+++ js/betyou5.js
@@ -0,0 +1,67 @@
+(function () {
+	var app = angular.module('betYou5',[]);
+
+	app.controller('GameController', ['$scope', function($scope){
+
+		$scope.namePlayer1 = "";
+		$scope.namePlayer2 = "";
+		$scope.scoreP1 = 0;
+		$scope.scoreP2 = 0;
+		$scope.maxScore = 200;
+		$scope.dice1 = 0;
+		$scope.dice2 = 0;
+		$scope.playerTurn = false;
+		$scope.playerNameTurn = $scope.namePlayer2;
+
+		$scope.rollDice = function(){
+
+			var newDice1 = Math.floor((Math.random() * 6) + 1);
+			var newDice2 = Math.floor((Math.random() * 6) + 1);
+
+			$scope.diceTotal = newDice1 + newDice2;
+
+			$scope.dice1 = newDice1;
+			$scope.dice2 = newDice2;
+			
+			
+
+			// Change between both player and sums according it player.
+			var newPlayerTurn = false;
+			
+			if ($scope.playerTurn === false){
+				$scope.scoreP1 = $scope.scoreP1 + $scope.diceTotal;
+				newPlayerTurn = true;
+				$scope.playerTurn = newPlayerTurn;
+				$scope.playerNameTurn = $scope.namePlayer2;
+			}
+			else if ($scope.playerTurn === true){
+				$scope.scoreP2 = $scope.scoreP2 + $scope.diceTotal;
+				newPlayerTurn = false;
+				$scope.playerTurn = newPlayerTurn;
+				$scope.playerNameTurn = $scope.namePlayer1;
+			}
+
+
+
+			if ($scope.scoreP1 >= $scope.maxScore || $scope.scoreP2 >= $scope.maxScore )
+			{
+				$scope.mainMenu = 'stats';
+			}
+		}
+
+		$scope.gameStats = function(){
+
+				$scope.scoreP1 = 0;
+				$scope.scoreP2 = 0;
+				$scope.maxScore = 200;
+				$scope.namePlayer1 = "";
+				$scope.namePlayer2 = "";
+				$scope.mainMenu = 'index';
+		}
+
+		$scope.rollDice();
+		$scope.gameStats();
+	
+
+	}]);
+})();
